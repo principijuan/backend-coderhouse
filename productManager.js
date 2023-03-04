ea837d00056237bd84055022905bf5da4537d948
Juan Principi <principijuan@gmail.com>
2023-03-04 19:08:55 -0300

    first commit

---
+++ productManager.js
@@ -0,0 +1,49 @@
+productsArray = [{
+    id: 1,
+    title: "Nombre del producto",
+    description: "Descripción del producto",
+    price: 100,
+    thumbnail: "https://lh3.google.com/u/0/d/1_klL0hfgL8wMXINYZBxRN5aAvw0lH3eN=w1920-h937-iv1",
+    code: "0001",
+    stock: 100
+}]
+
+class ProductManager {
+    constructor(){
+        this.products = productsArray
+    }
+    addProduct(newProduct){
+        const product = this.products.find(prod => prod.code === newProduct.code)
+        if(product){
+            return "Existe el producto con este código"
+        }
+        this.products.push({... newProduct, id: this.products.length + 1})
+    }
+    getProducts(){
+        return this.products
+    }
+    getProductById(id){
+        const product = this.products.find(prod => prod.id === id)
+        if (!product){
+            return "Not found"
+        }
+        return product
+    }
+}
+
+// module.exports = {
+//     ProductManager
+// }
+
+const productos = new ProductManager()
+productos.addProduct({
+    id: 1,
+    title: "Nombre del producto 2",
+    description: "Descripción del producto 2",
+    price: 200,
+    thumbnail: "https://lh3.google.com/u/0/d/1_klL0hfgL8wMXINYZBxRN5aAvw0lH3eN=w1920-h937-iv1",
+    code: "0002",
+    stock: 100
+})
+console.log("todos", productos.getProducts())
+console.log("por id", productos.getProductById(2))
